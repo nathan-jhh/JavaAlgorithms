50b171524d1b6d6a40a301b0fe3e457521c04e02
nathan-jhh <jhaoheng@163.com>
2020-10-03 00:23:54 +0800

    删除元素

--- DataStructures/src/BSTTest.java
+++ DataStructures/src/BSTTest.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class BSTTest {
     public static void main(String[] args) {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
@@ -25,5 +28,28 @@ public class BSTTest {
 
         System.out.print("\nmin: " + bst.min());
         System.out.print("\nminNR: " + bst.minNR());
+
+        //deleteMin  Test
+        Random random = new Random();
+        BinarySearchTree<Integer> bst2 = new BinarySearchTree<>();
+        for (int i = 0; i < 10000; i++) {
+            bst2.add(random.nextInt(10000)); //向二分搜索树中随机添加小于10000的数
+        }
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        while (!bst2.isEmpty()) {
+            arrayList.add(bst2.deleteMin()); //不出意外的话，arrayList里边的数据应该是从小到大排列的
+        }
+        System.out.println("\narrayList: " + arrayList);
+        for (int i = 1; i < arrayList.size(); i++) {
+            if (arrayList.get(i - 1) > arrayList.get(i)) {
+                throw new IllegalArgumentException("二分搜索树实现有问题！");
+            }
+        }
+        System.out.println("deleteMin success");
+
+        //delete Test
+        bst.delete(5);
+        System.out.print("\n删除后再中序遍历: ");
+        bst.preOrder();
     }
 }
--- DataStructures/src/BinarySearchTree.java
+++ DataStructures/src/BinarySearchTree.java
@@ -173,9 +173,111 @@ public class BinarySearchTree<E extends Comparable<E>> {
 
     //寻找二分搜索树中最小的元素,非递归写法，Not Recursive
     public E minNR() {
-        while (root.left != null) {
-            root = root.left;
+        Node curNode = root; //很重要，不能直接用root
+        while (curNode.left != null) {
+            curNode = curNode.left;
         }
-        return root.e;
+        return curNode.e;
+    }
+
+    // 寻找二分搜索树的最大元素
+    public E max() {
+        if (size == 0) {
+            throw new IllegalArgumentException("BST is empty!");
+        }
+        return max(root).e;
+    }
+
+    // 返回以node为根的二分搜索树的最大值所在的节点
+    private Node max(Node node) {
+        if (node.right == null)
+            return node;
+        return max(node.right);
+    }
+
+    //删除二分搜索树中的最小元素所在节点，并返回最小元素,递归写法
+    public E deleteMin() {
+        E res = min();
+        root = deleteMin(root);
+        return res;
+    }
+
+    //删除掉以node为根的二分搜索树中的最小节点，并返回删除节点后新的二分搜索树的根
+    private Node deleteMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = deleteMin(node.left);
+        return node;
+    }
+
+    // 从二分搜索树中删除最大值所在节点,返回最大元素
+    public E deleteMax() {
+        E ret = max();
+        root = deleteMax(root);
+        return ret;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最大节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node deleteMax(Node node) {
+
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+
+        node.right = deleteMax(node.right);
+        return node;
+    }
+
+    //从二分搜索树中删除指定元素e
+    public void delete(E e) {
+        root = delete(root, e);
+    }
+
+    //从以node为根的二分搜索树中删除指定元素所在节点，递归算法
+    // 返回删除节点后新的二分搜索树的根
+    public Node delete(Node node, E e) {
+        if (node == null) {
+            return null;
+        }
+        if (e.compareTo(node.e) < 0) {
+            node.left = delete(node.left, e);
+            return node;
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = delete(node.right, e);
+            return node;
+        } else {    //此时就是要删除这个节点
+            if (node.left == null) {
+                Node rightNode = node.right;
+                node.right = null;
+                size--;
+                return rightNode;
+            }
+            if (node.right == null) {
+                Node leftNode = node.left;
+                node.left = null;
+                size--;
+                return leftNode;
+            }
+
+            //左右子树都不为空的情况
+
+            //找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+            //用这个节点顶替待删除节点的位置
+            Node node1 = min(node.right);
+            node1.right = deleteMin(node.right);
+            node1.left = node.left;
+            //size--; //在这里是不需要维护size的，因为在函数deleteMin(Node node)中已经维护了size
+            node.left = null;
+            node.right = null;
+            return node1;
+        }
     }
 }
