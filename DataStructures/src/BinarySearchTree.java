1cd4907e9e7d0d8280560fd6e3233e2c60958cef
nathan-jhh <jhaoheng@163.com>
2020-10-02 21:01:40 +0800

    层序遍历

--- DataStructures/src/BinarySearchTree.java
+++ DataStructures/src/BinarySearchTree.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BinarySearchTree<E extends Comparable<E>> {
     //不包含重复元素的二分搜索树
 
@@ -43,19 +47,20 @@ public class BinarySearchTree<E extends Comparable<E>> {
         }
         if (e.compareTo(node.e) < 0) {
             node.left = add(node.left, e);
-        } else if (e.compareTo(node.e) > 0){
+        } else if (e.compareTo(node.e) > 0) {
             node.right = add(node.right, e);
         }
         return node;
     }
 
     //查询二分搜索树中是否包含元素e
-    public boolean contains(E e){
+    public boolean contains(E e) {
         return contains(root, e);
     }
+
     //查询以node为根节点的二分搜索树中是否包含元素e
-    private boolean contains(Node node, E e){
-        if (node == null){
+    private boolean contains(Node node, E e) {
+        if (node == null) {
             return false;
         }
         if (e.compareTo(node.e) == 0) {
@@ -67,29 +72,46 @@ public class BinarySearchTree<E extends Comparable<E>> {
         }
     }
 
-    //前序遍历
-    public void preOrder(){
+    //前序遍历,深度优先遍历
+    public void preOrder() {
         preOrder(root);
     }
-    private void preOrder(Node node){
-        if (node == null){
+
+    private void preOrder(Node node) {
+        if (node == null) {
             return;
         }
 
-        //遍历操作
+        //操作
         System.out.print(node.e + " ");
 
         preOrder(node.left);
         preOrder(node.right);
     }
 
+    // 二分搜索树的非递归前序遍历实现
+    public void preOrderNR() {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            System.out.print(cur.e + " ");
+
+            if (cur.right != null)
+                stack.push(cur.right);
+            if (cur.left != null)
+                stack.push(cur.left);
+        }
+    }
+
     //中序遍历,遍历后的结果有序的
-    public void inOrder(){
+    public void inOrder() {
         inOrder(root);
     }
+
     // 中序遍历以node为根的二分搜索树, 递归算法
-    private void inOrder(Node node){
-        if(node == null)
+    private void inOrder(Node node) {
+        if (node == null)
             return;
 
         inOrder(node.left);
@@ -101,13 +123,13 @@ public class BinarySearchTree<E extends Comparable<E>> {
     }
 
     //后序遍历
-    public void postOrder(){
+    public void postOrder() {
         postOrder(root);
     }
 
     //后序遍历以node为根的二分搜索树, 递归算法
-    private void postOrder(Node node){
-        if(node == null)
+    private void postOrder(Node node) {
+        if (node == null)
             return;
         postOrder(node.left);
         postOrder(node.right);
@@ -115,4 +137,23 @@ public class BinarySearchTree<E extends Comparable<E>> {
         //遍历操作
         System.out.print(node.e + " ");
     }
+
+    //层序遍历，广度优先遍历
+    public void levelOrder() {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            //遍历操作
+            System.out.print(node.e + " ");
+            if (node.left != null) {
+                q.add(node.left);
+            }
+            if (node.right != null) {
+                q.add(node.right);
+            }
+        }
+    }
+
+
 }
